5a32c8d3aa0011eab0f29481c9322078341ed3f3
Aaron Espinosa Asencio <espinosa.asencio.aaron@gmail.com>
2022-05-09 19:09:37 +0200

    Add files via upload

---
+++ checkURLstatus.ps1
@@ -0,0 +1,11 @@
+﻿$domains = Get-Content #here your domain file path
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+foreach($domain in $domains) {
+    Try{
+        Invoke-WebRequest -Method Head -Uri $domain -UseBasicParsing
+        echo $dominio >> #output path of 200ok domains
+        }
+    Catch{
+        echo "$dominio : $_" >> #output path of not 200ok domains
+        }
+}
---
+++ resolveDomains.ps1
@@ -0,0 +1,10 @@
+﻿foreach($domain in Get-Content #here your domain file path) {
+    try{
+        $ip = [System.Net.Dns]::GetHostAddresses($domain);
+        echo $dominio": "$ip >> #output path of domains that resolves;
+    }
+    catch{
+        echo $dominio >> #output path of domains that does not resolve;
+    }
+
+}
